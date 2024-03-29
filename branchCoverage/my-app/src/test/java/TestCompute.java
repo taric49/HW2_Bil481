78cc0a04a3b9083f84c43437f154efe20e19ecc3
taric49 <tderici@edu.edu.tr>
2024-03-29 14:23:42 +0300

    completed Compute.java coverage

--- branchCoverage/my-app/src/test/java/TestCompute.java
+++ branchCoverage/my-app/src/test/java/TestCompute.java
@@ -11,14 +11,31 @@ public class TestCompute {
   @Test
   public void example() {
     MessageQueue mq = mock(MessageQueue.class);
+    when(mq.size()).thenReturn(0);
     c = new Compute(mq);
-    c.countNumberOfOccurrences("");
+    int result = c.countNumberOfOccurrences("");
+    assertTrue(result == -1);
   }
 
   @Test
   public void test1() {
     MessageQueue mq = mock(MessageQueue.class);
     c = new Compute(mq);
-    c.countNumberOfOccurrences("Hello World!");
+    when(mq.size()).thenReturn(2);
+    when(mq.contains("A")).thenReturn(true);
+    when(mq.getAt(0)).thenReturn("A");
+    when(mq.getAt(1)).thenReturn("B");
+    int result = c.countNumberOfOccurrences("A");
+    assertTrue(result == 1);
+  }
+
+  @Test
+  public void test2() {
+    MessageQueue mq = mock(MessageQueue.class);
+    c = new Compute(mq);
+    when(mq.size()).thenReturn(1);
+    when(mq.contains("A")).thenReturn(false);
+    int result = c.countNumberOfOccurrences("B");
+    assertTrue(result == 0);
   }
 }

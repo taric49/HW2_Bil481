e02ee71fe4fa64571ee7d2e9af321d10e229d741
taric49 <tderici@edu.edu.tr>
2024-03-29 14:38:55 +0300

    completed Util.java covarage

--- branchCoverage/my-app/src/test/java/TestUtil.java
+++ branchCoverage/my-app/src/test/java/TestUtil.java
@@ -9,8 +9,32 @@ public class TestUtil {
   Util c;
 
   @Before
-  public void setUp() { c = new Util(); }
+  public void setUp() {
+    c = new Util();
+  }
 
   @Test
-  public void example() { assertTrue(true); }
+  public void test0() {
+    assertFalse(c.compute(1));
+  }
+
+  @Test
+  public void test1() {
+    assertFalse(c.compute(1, 2));
+  }
+
+  @Test
+  public void test2() {
+    assertTrue(c.compute(2, 4, 6));
+  }
+
+  @Test
+  public void test3() {
+    assertFalse(c.compute(11, 5, 7));
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void test4() {
+    assertFalse(c.compute(0, 4, 6));
+  }
 }
